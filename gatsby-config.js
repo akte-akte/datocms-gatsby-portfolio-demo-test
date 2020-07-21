398a99aefe311b53c71cbcf156d9433cff2a2011
akte-akte <kateahogden@gmail.com>
2020-07-21 22:48:57 +1000

    feat(cart): add snipcart plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,14 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-snipcartv3',
+      options: {
+        apiKey: 'MmQyZWYwMzQtMmMwYi00NDNiLTkwMTktZjU0MTdlNmNkZTBkNjM3Mjk2ODA4NzkyMTExODI2',
+        js: true,
+        css: true,
+      }
+    },
+    {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: [
