c03be88730cacf301a5990e81d1d71b399addba5
akte-akte <kateahogden@gmail.com>
2020-07-23 17:41:32 +1000

    feat(about): change photo field to gallery

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,29 +1,54 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
-import Img from 'gatsby-image'
+import { GridList, GridListTile } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Layout from "../components/layout"
 
-const About = ({ data: { about } }) => (
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+}));
+
+const About = ({ data: { about } }) => {
+  const classes = useStyles();
+
+  return (
+
   <Layout>
     <article className="sheet">
       <HelmetDatoCms seo={about.seoMetaTags} />
       <div className="sheet__inner">
         <h1 className="sheet__title">{about.title}</h1>
         <p className="sheet__lead">{about.subtitle}</p>
-        <div className="sheet__gallery">
-          <Img fluid={about.photo.fluid} />
-        </div>
+
         <div
           className="sheet__body"
           dangerouslySetInnerHTML={{
             __html: about.bioNode.childMarkdownRemark.html,
           }}
         />
+        <GridList cellHeight={160} className={classes.gridList} cols={3}>
+=          {about.photos.map((photo) => (
+            <GridListTile key={photo.filename} cols={photo.cols || 1}>
+              <img src={photo.url} alt={photo.alt} />
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
     </article>
   </Layout>
-)
+  )
+          };
 
 export default About
 
@@ -35,7 +60,10 @@ export const query = graphql`
       }
       title
       subtitle
-      photo {
+      photos {
+        alt
+        url
+        filename
         fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
           ...GatsbyDatoCmsSizes
         }
