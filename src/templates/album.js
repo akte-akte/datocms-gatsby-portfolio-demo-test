c881983c2dd8b606b21721f0e19710a71587b222
akte-akte <kateahogden@gmail.com>
2020-07-23 21:47:18 +1000

    feat(album):  listing tracks on album

--- src/templates/album.js
+++ src/templates/album.js
@@ -3,10 +3,8 @@ import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
-import { Button, Card, Grid, CardContent, CardMedia, Typography, IconButton } from '@material-ui/core';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import { Button, Card, Grid, CardContent, CardMedia, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import AudioPlayer from 'material-ui-audio-player';
  
@@ -15,6 +13,56 @@ const muiTheme = createMuiTheme({});
 const useStyles = makeStyles((theme) => ({
   contentPadding: {
     margin: '20px 0px',
+  },
+  root: {
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
+  },
+  loopIcon: {
+    color: '#3f51b5',
+    '&.selected': {
+      color: '#0921a9',
+    },
+    '&:hover': {
+      color: '#7986cb',
+    },
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  playIcon: {
+    color: '#f50057',
+    '&:hover': {
+      color: '#ff4081',
+    },
+  },
+  replayIcon: {
+    color: '#e6e600',
+  },
+  pauseIcon: {
+    color: '#0099ff',
+  },
+  volumeIcon: {
+    color: 'rgba(0, 0, 0, 0.54)',
+  },
+  volumeSlider: {
+    color: 'black',
+  },
+  progressTime: {
+    color: 'rgba(0, 0, 0, 0.54)',
+  },
+  mainSlider: {
+    color: '#3f51b5',
+    '& .MuiSlider-rail': {
+      color: '#7986cb',
+    },
+    '& .MuiSlider-track': {
+      color: '#3f51b5',
+    },
+    '& .MuiSlider-thumb': {
+      color: '#303f9f',
+    },
   },
 }));
 
@@ -39,7 +87,7 @@ export default ({ data }) => {
           <Button
             variant="contained"
             color="primary"
-            class="snipcart-add-item"
+            className="snipcart-add-item"
             data-item-id={data.album.id}
             data-item-price={data.album.price.toFixed(2)}
             data-item-url={`https://practical-hugle-a440b6.netlify.app/albums/${data.album.slug}`}
@@ -56,8 +104,8 @@ export default ({ data }) => {
             <Img fluid={data.album.albumArtwork.fluid} />
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                {data.album.tracks.map((track, id) => (
-                  <div key={track.id}>
+                {data.album.tracks.map((track) => (
+                  <div key={track.id} id={`#${track.title.toLowerCase().replace(/\s+/g, '')}`}>
                     <Card className={classes.contentPadding} square>
                       <Grid container spacing={3}>
                         <Grid item xs={12} sm={6} md={3}>
@@ -65,38 +113,49 @@ export default ({ data }) => {
                             component="img"
                             image={track.artwork.url}
                             title={track.title}
-                            maxHeight="140"
+                            maxheight="140"
                           />
                         </Grid>
                         <Grid item xs={12} sm={6} md={9}>
                             <CardContent>
+                              <Grid container>
+                            <Grid item xs={12} sm={6}>
                               <Typography variant="h5" component="h2">
                                 {track.title}
                               </Typography>
+                              </Grid>
+                              <Grid item xs={12} sm={6}>
                               <Typography variant="body2" component="p">
                                 ${track.price.toFixed(2)}AUD
                               </Typography>
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                className="snipcart-add-item"
+                                data-item-file-guid={track.cartGuid}
+                                data-item-id={track.id}
+                                data-item-name={track.title}
+                                data-item-price={track.price}
+                                data-item-url={`https://practical-hugle-a440b6.netlify.app/albums/${data.album.slug}#${track.title}`}
+
+                              >
+                                Add to cart
+                            </Button>
+                            </Grid>
+                            </Grid>
                               <ThemeProvider theme={muiTheme}>
                                <AudioPlayer src={track.trackSample.url}
                                 elevation={0}
                                 width="100%"
                                 variation="default"
-                                spacing={3}
+                                spacing={2}
                                 download={false}
                                 autoplay={false}
                                 order="standart"
                                 preload="auto"
-                                loop={false} />
+                                loop={false}
+                                useStyles={useStyles} />
                             </ThemeProvider>
-      
-                              <Button
-                                variant="contained"
-                                color="primary"
-                                class="snipcart-add-item"
-                                data-item-file-guid={track.cartGuid}
-                              >
-                                Add to cart
-                              </Button>
                             </CardContent>
                         </Grid>
                       </Grid>
